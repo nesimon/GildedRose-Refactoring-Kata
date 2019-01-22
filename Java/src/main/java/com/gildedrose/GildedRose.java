201ceb53e144f4d9835d996072d9d09e3c8b34c7
SIMON Neil <nsimon@murex.com>
2019-01-22 16:30:06 +0000

    Added comments to main code without changes

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,6 +8,7 @@ class GildedRose {
     }
 
     public void updateQuality() {
+        //Reduces quality of normal items by 1 daily
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -16,17 +17,19 @@ class GildedRose {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
+                //if cheese or tickets, increase value by 1
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        //if only 10 days to concert, increase value by 1 more
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
-
+                        //if only 5 days to concert, increase value by 1 more if cost is less than 50
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -35,7 +38,7 @@ class GildedRose {
                     }
                 }
             }
-
+            //reduce days to sell by 1 to all but SULFURAS, HAND OF RAGNAROS!
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
@@ -45,12 +48,15 @@ class GildedRose {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                                //reduce cost of a regular item by 1 more if its expired
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
+                        //ticket value is 0 since it expired
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
+                    //cheese increases in value by 1 more since it is aged well
                 } else {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
